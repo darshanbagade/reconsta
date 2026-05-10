2ecbe6f2082e6a188ed445b24318118e0c1c6295
Darshan Bagade <darshanbagade123@gmail.com>
2026-05-10 19:09:02 +0530

    chore: setup Reconsta frontend skeleton

---
+++ reconsta-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+        <h1>Reconsta frontend</h1>
+        <p>Payment Reconciliation Intelligence Platform</p>
+    </>
+  )
+}
+
+export default App
